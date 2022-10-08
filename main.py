c247178b46d62bef1b942ac4ce01d17300981274
uulukbek <ulanovulukbek2@gmail.com>
2022-10-08 18:10:48 +0600

    finish

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ from views import *
 
 def main(): 
     while True: 
-        print('Здравствуйте, вот наш функционал: \n1 - Получить все товары \n2 - Получить определенный товар \n3 - опубликовать товар \n4 - удалить товар \n5 - обновить товар\n 0 - Выйти')
+        print('Здравствуйте, вот наш функционал: \n1 - Получить все товары \n2 - Получить определенный товар \n3 - опубликовать товар \n4 - удалить товар \n5 - обновить товар\n6 -сортировать товар\n 0 - Выйти')
 
         method_ = int(input('Выберайте: '))
         if method_ == 1: 
-            filter = True if input('Хотите отфильтровать? ').lower() == 'да' else False 
-            print(get_data(filter))
-
+           print(get_products())
         elif method_ == 2: 
             print(get_product(int(input('Введите id продукта: '))))
         elif method_ == 3: 
@@ -17,6 +15,9 @@ def main():
             print(delete_product(int(input('Введите id продукта: '))))
         elif method_ == 5: 
             print(update_product(int(input('Введите id продукта'))))
+        elif method_ == 6: 
+            filter = True if input('Хотите сортировать? ').lower() == 'да' else False 
+            print(get_data(filter))
         elif method_ == 0: 
             break
         else: 
